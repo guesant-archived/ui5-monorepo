670ff4cefec457c126f77aca5d6323173a5edb3c
Gabriel Rodrigues <jipacoding@gmail.com>
2020-08-31 11:13:02 -0400

    feat(react-user-interface): InputRadioGroup

---
+++ packages/react-user-interface/src/Form/InputRadioGroup.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import styled from "styled-components";
+
+export interface InputRadioGroupProps
+  extends React.HTMLAttributes<HTMLDivElement> {}
+
+export const StyledInputRadioGroup = styled.div``;
+
+export const InputRadioGroup = StyledInputRadioGroup;
