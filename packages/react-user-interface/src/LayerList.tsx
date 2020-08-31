2af17dab8950e21b3446ba14bd6f13fcdb04aab2
Gabriel Rodrigues <jipacoding@gmail.com>
2020-08-31 10:07:47 -0400

    feat(react-user-interface): LayerList

---
+++ packages/react-user-interface/src/LayerList.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import styled from "styled-components";
+
+export interface LayerListProps
+  extends React.HTMLAttributes<HTMLUListElement> {}
+
+export const StyledLayerList = styled.ul``;
+
+export const LayerList = StyledLayerList;
